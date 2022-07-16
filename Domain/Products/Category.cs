bcc5609412c87b3453ffcb60bb8a67755bfe1b52
ffdadalto <ffdadalto@gmail.com>
2022-07-16 10:53:15 -0300

    Created Entity class

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,8 +1,6 @@
 ﻿namespace IWantApp.Domain.Products
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+    public class Category : Entity
+    {        
     }
 }
--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -1,9 +1,7 @@
 ﻿namespace IWantApp.Domain.Products
 {
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+    public class Product : Entity
+    {        
         public Category Category { get; set; }
         public string Description { get; set; }
         public bool HasStock { get; set; }
